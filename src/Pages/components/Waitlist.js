049673fca9680951c549fd02b406035b5cfe0255
Hyunjun Lee <hjl1013@snu.ac.kr>
2024-09-12 23:01:08 +0900

    organized waitlist component

--- src/Pages/components/Waitlist.js
+++ src/Pages/components/Waitlist.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './Waitlist.css'
 
-function WaitlistMain() {
+function WaitlistMain({setisWaitlist, setPopupState}) {
     return (
-        <div className='waitlistMain'>
+        <div className='waitlistMain white_background'>
             <div className='waitlistMain__header'>
                 <p className='waitlistMain__title poppins-semibold'>Waitlist</p>
                 <p className='waitlistMain__subtitle poppins-medium'>We will be launching our product in January, 2025.<br/>If you are interested, book ROUND or leave your e-mail to follow up with us!</p>
@@ -18,7 +18,7 @@ function WaitlistMain() {
                             <p className='poppins-medium dark1'>Get followup news</p>
                         </div>
                     </div>
-                    <div className='waitlistMain__planButton dark1_background'>
+                    <div onClick={() => setPopupState(1)} className='waitlistMain__planButton dark1_background'>
                         <p className='white poppins-semibold'>Leave e-mail</p>
                     </div>
                 </div>
@@ -39,19 +39,26 @@ function WaitlistMain() {
                             <p className='poppins-medium white'>Get followup news</p>
                         </div>
                     </div>
-                    <div className='waitlistMain__planButton white_background'>
+                    <div onClick={() => setPopupState(2)} className='waitlistMain__planButton white_background'>
                         <p className='dark1 poppins-semibold'>Start Booking</p>
                     </div>
                 </div>
             </div>
+
+            <img onClick={() => {setisWaitlist(false); setPopupState(0);}} className='waitlist__exit' alt='' src={process.env.PUBLIC_URL + '/icons/exit.svg'}/>
         </div>
     )
 }
 
-function WaitlistEmail() {
+function WaitlistEmail({setPopupState}) {
     return (
-        <div className='waitlistEmail'>
-            email
+        <div className='waitlistEmail white_background'>
+            <div className='waitlistEmail__header'>
+                <p className='waitlistEmail__title poppins-semibold'>Leave E-mail</p>
+                <p className='waitlistEmail__subtitle poppins-medium'>Leave your email, and we will update you with exciting news about ROUND.</p>
+            </div>
+
+            <img onClick={() => setPopupState(0)} className='waitlist__exit' alt='' src={process.env.PUBLIC_URL + '/icons/exit.svg'}/>
         </div>
     )
 }
@@ -60,16 +67,14 @@ function Waitlist({setisWaitlist}) {
     const [popupState, setPopupState] = useState(1);
 
     return (
-        <div className="waitlist white_background">
+        <div className="waitlist">
             <div className={`waitlist__main ${popupState === 0 ? 'waitlist__main--show' : 'waitlist__main--hide'}`}>
-                <WaitlistMain />
+                <WaitlistMain setisWaitlist={x => setisWaitlist(x)} setPopupState={x => setPopupState(x)} />
             </div>
 
             <div className={`waitlist__email ${popupState === 1 ? 'waitlist__email--show' : 'waitlist__email--hide'}`}>
-                <WaitlistEmail />
+                <WaitlistEmail setisWaitlist={x => setisWaitlist(x)} setPopupState={x => setPopupState(x)} />
             </div>
-
-            <img onClick={() => {setisWaitlist(false); setPopupState(0);}} className='waitlist__exit' alt='' src={process.env.PUBLIC_URL + '/icons/exit.svg'}/>
         </div>
     )
 }
