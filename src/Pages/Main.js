dea78aa1b6850b2f7f27e53460e6fe440595a7ee
Hyunjun Lee <hjl1013@snu.ac.kr>
2024-09-19 00:53:17 +0900

    added visualization, multi environment features

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -7,9 +7,10 @@ function Main() {
     return (
         <div className='main'>
             <div className='main__header white_background'>
-                <img src={process.env.PUBLIC_URL + '/logo/logo.svg'} alt=''/>
+                <a className='main__logoLink' href='#starting'><img src={process.env.PUBLIC_URL + '/logo/logo.svg'} alt=''/></a>
                 <div className='main__menus'>
                     <a href='#starting'><p className='main__menu poppins-medium'>HOME</p></a>
+                    <a href='#features'><p className='main__menu poppins-medium'>FEATURES</p></a>
                     <a href='#product'><p className='main__menu poppins-medium'>PRODUCT</p></a>
                     <a href='#partners'><p className='main__menu poppins-medium'>PARTNERS</p></a>
                     <a href='#contact'><div className='main__headerContact blue2_background'>
@@ -19,8 +20,8 @@ function Main() {
             </div>
 
             <div className='main__starting' id='starting'>
-                <p className='main__startingSlogan suse-bold'>Easy Data Preparation</p>
-                <p className='main__startingExplanation poppins-regular'>Empower your data journey with high flexibility,<br/>easy visualization, and a developer-friendly data preparation tool</p>
+                <p className='main__startingSlogan suse-bold'>Visualize Your Data Workflows</p>
+                <p className='main__startingExplanation poppins-regular'>Reinvent your data journey with high flexibility,<br/>easy visualization, and a developer-friendly data preparation tool</p>
                 <div onClick={() => {console.log(isWaitlist); setIsWaitlist(true);}} className='main__waitlistButton blue2_background'>
                     <p className='poppins-medium white'>Join Waitlist</p>
                 </div>
@@ -49,19 +50,31 @@ function Main() {
                 </div>
             </div>
 
-            <div className='main__visualization main__body'>
-                <div className='main__visualizationTitles'>
-                    <p className='main__visualizationsubTitle poppins-medium'>Monitor data changes throughout your pipeline</p>
-                    <p className='main__visualizationTitle suse-bold'>Easily Visualize your Data Process</p>
+            <div className='main__featuresContainer dark4_background' id='features'>
+                <div className='main__visualization'>
+                    <div className='main__visualizationTitles'>
+                        <p className='main__visualizationsubTitle poppins-medium'>Monitor data changes throughout your pipeline</p>
+                        <p className='main__visualizationTitle suse-bold'>Easily Visualize your Data Process</p>
+                    </div>
+                    <video className='main__visualizationVideo' autoPlay={true} loop muted>
+                        <source src={process.env.PUBLIC_URL + '/videos/visualization.mp4'} type='video/mp4'/>
+                    </video>
                 </div>
-                <video className='main__visualizationVideo' autoPlay={true} loop muted>
-                    <source src={process.env.PUBLIC_URL + '/videos/visualization.mp4'} type='video/mp4'/>
-                </video>
+
+                <div className='main__multienv'>
+                    <video className='main__multienvVideo' autoPlay={true} loop muted>
+                        <source src={process.env.PUBLIC_URL + '/videos/multienv.mp4'} type='video/mp4'/>
+                    </video>
+                    <div className='main__multienvTitles'>
+                        <p className='main__multienvsubTitle poppins-medium'>Resolve compatibility issues with multi environments</p>
+                        <p className='main__multienvTitle suse-bold'>Set Multi Environments</p>
+                    </div>
+                </div>
             </div>
 
-            <div className='main__body1 main__body' id='product'>
+            <div className='main__body1' id='product'>
                 <div className='main__body1Text'>
-                    <p className='main__body1TextTitle suse-bold'>Amazing User<br/>Experience with<br/>ROUND</p>
+                    <p className='main__body1TextTitle suse-bold'>Code Smarter<br/>Visualize Faster</p>
                     <div className='main__body1TextSteps'>
                         <p className='main__body1TextStep poppins-medium'><span className='blue1'>Step 1.</span> Forking</p>
                         <p className='main__body1TextStep poppins-medium'><span className='blue1'>Step 2.</span> Customizing</p>
@@ -105,12 +118,14 @@ function Main() {
                 </div>
             </div>
 
-            <div className='main__partners' id='partners'>
-                <p className='main__partnersTitle suse-bold'>Our Partners</p>
-                <div className='main__partnerLogos'>
-                    <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/sim2real.svg'}/>
-                    <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/story.png'}/>
-                    <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/vessl.png'}/>
+            <div className='main__partnersContainer dark4_background' id='partners'>
+                <div className='main__partners'>
+                    <p className='main__partnersTitle suse-bold'>Our Partners</p>
+                    <div className='main__partnerLogos'>
+                        <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/sim2real.svg'}/>
+                        <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/story.png'}/>
+                        <img className='main__partnerLogo' alt='' src={process.env.PUBLIC_URL + '/partners/vessl.png'}/>
+                    </div>
                 </div>
             </div>
 
